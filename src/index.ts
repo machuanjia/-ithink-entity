9493d64bce99d175419d5a2e84eca07d568a279a
DayYoung <machuanjia@laiye.com>
2021-11-23 19:36:02 +0800

    feat: change

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 /*
  * @Author: D.Y.M
  * @Date: 2021-11-23 19:14:11
- * @LastEditTime: 2021-11-23 19:25:42
+ * @LastEditTime: 2021-11-23 19:31:13
  * @FilePath: /ithink-entity/src/index.ts
  * @Description:
  */
-export { default as initEntityApis } from './api'
-export { default as EntityAdapter } from './entity'
+export { default as initEntityApis } from './api';
+export { default as EntityAdapter } from './entity';
+export * from './request';
